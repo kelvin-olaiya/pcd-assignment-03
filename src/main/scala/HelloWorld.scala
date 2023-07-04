0a5d22d123404781a2a44f4d1d5f4583d3d02c8f
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-07-04 09:40:45 +0200

    chore: refactor

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -26,24 +26,6 @@ object DirectoryAnalyzer:
   }
 */
 
-class Leaderboard(
-  private val leaderboard: TreeSet[(String, Int)],
-  private val numLongestFiles: Int) {
-
-  def submit(path: String, lines: Int): Leaderboard =
-    val tempLeaderboard = leaderboard + (path -> lines)
-    if (leaderboard.size < numLongestFiles) {
-      Leaderboard(tempLeaderboard, numLongestFiles)
-    } else {
-      Leaderboard(tempLeaderboard - leaderboard.minBy(_._2), numLongestFiles)
-    }
-
-  def toList: List[(String, Int)] = leaderboard.toList.sorted((a,b) => b._2 - a._2)
-
-  def merge(leaderboard: Leaderboard): Leaderboard =
-    Leaderboard((this.toList ++ leaderboard.toList).take(numLongestFiles).to(TreeSet), numLongestFiles)
-}
-
 /*object SourceAnalyzer:
   sealed trait Command
   case class Count(path: String) extends Command
