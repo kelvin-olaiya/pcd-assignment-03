60a7bec972dffb13a5b014477483387fcb1f287c
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-07-10 10:37:29 +0200

    refactor: better naming

--- src/main/scala/pcd/assignment03/ex3/Main.scala
+++ src/main/scala/pcd/assignment03/ex3/Main.scala
@@ -14,20 +14,20 @@ import scala.util.Random
 
 
 trait ModelService extends Remote:
-  @throws(classOf[RemoteException]) def join(uuid: String, brush: Brush): java.util.Map[RemoteUser, Brush]
+  @throws(classOf[RemoteException]) def join(uuid: String, brush: Brush): java.util.Map[ModelObserver, Brush]
   @throws(classOf[RemoteException]) def getGrid: PixelGrid
-  @throws(classOf[RemoteException]) def userChangeColor(remote: RemoteUser, color: Int): Unit
-  @throws(classOf[RemoteException]) def color(remote: RemoteUser, x: Int, y: Int): Unit
-  @throws(classOf[RemoteException]) def leave(remote: RemoteUser): Unit
+  @throws(classOf[RemoteException]) def userChangeColor(remote: ModelObserver, color: Int): Unit
+  @throws(classOf[RemoteException]) def color(remote: ModelObserver, x: Int, y: Int): Unit
+  @throws(classOf[RemoteException]) def leave(remote: ModelObserver): Unit
 
 case class ModelServiceImpl() extends ModelService:
-  val users: TrieMap[RemoteUser, Brush] = TrieMap[RemoteUser, Brush]()
+  val users: TrieMap[ModelObserver, Brush] = TrieMap[ModelObserver, Brush]()
   val grid: PixelGrid = PixelGrid(40, 40)
   val registry: Registry = LocateRegistry.getRegistry(null)
 
-  def join(uuid: String, brush: Brush): java.util.Map[RemoteUser, Brush] = synchronized {
-    val remote = registry.lookup(uuid).asInstanceOf[RemoteUser]
-    users.foreach((u, _) => u.newUserJoin(remote, brush))
+  def join(uuid: String, brush: Brush): java.util.Map[ModelObserver, Brush] = synchronized {
+    val remote = registry.lookup(uuid).asInstanceOf[ModelObserver]
+    users.foreach((u, _) => u.onUserJoin(remote, brush))
     val copy = Map.from(users)
     users.put(remote, brush)
     scala.jdk.javaapi.CollectionConverters.asJava(copy)
@@ -35,17 +35,17 @@ case class ModelServiceImpl() extends ModelService:
 
   def getGrid: PixelGrid = grid
 
-  def userChangeColor(remote: RemoteUser, color: Int): Unit = synchronized {
+  def userChangeColor(remote: ModelObserver, color: Int): Unit = synchronized {
     users(remote).setColor(color)
   }
 
-  def color(remote: RemoteUser, x: Int, y: Int): Unit = synchronized {
+  def color(remote: ModelObserver, x: Int, y: Int): Unit = synchronized {
     val color = users(remote).getColor
     grid.set(x, y, color)
-    users.foreach((u, _) => u.color(x, y, color))
+    users.foreach((u, _) => u.onPixelColor(x, y, color))
   }
 
-  def leave(remote: RemoteUser): Unit = synchronized {
+  def leave(remote: ModelObserver): Unit = synchronized {
     users.remove(remote)
   }
 
@@ -55,52 +55,52 @@ object Main extends App:
   model.registry.rebind("modelService", remoteModel)
   println("Server created!")
 
-trait RemoteUser extends Remote:
-  @throws(classOf[RemoteException]) def newUserJoin(remote: RemoteUser, brush: Brush): Unit
-  @throws(classOf[RemoteException]) def userMovedMouse(remote: RemoteUser, x: Int, y: Int): Unit
-  @throws(classOf[RemoteException]) def userChangeColor(remote: RemoteUser, color: Int): Unit
-  @throws(classOf[RemoteException]) def color(x: Int, y: Int, color: Int): Unit
-  @throws(classOf[RemoteException]) def userLeave(remote: RemoteUser): Unit
+trait ModelObserver extends Remote:
+  @throws(classOf[RemoteException]) def onUserJoin(remote: ModelObserver, brush: Brush): Unit
+  @throws(classOf[RemoteException]) def onMouseMoved(remote: ModelObserver, x: Int, y: Int): Unit
+  @throws(classOf[RemoteException]) def onUserColorChange(remote: ModelObserver, color: Int): Unit
+  @throws(classOf[RemoteException]) def onPixelColor(x: Int, y: Int, color: Int): Unit
+  @throws(classOf[RemoteException]) def onUserExit(remote: ModelObserver): Unit
 
 trait LocalUser:
   def userId: UUID
   def brush: Brush
 
-case class UserImpl() extends RemoteUser with LocalUser:
+case class UserImpl() extends ModelObserver with LocalUser:
   val userId: UUID = UUID.randomUUID()
   val brush: Brush = Brush(0, 0, Random.nextInt(256 * 256 * 256))
   val brushManager: BrushManager = BrushManager()
-  var remoteUsers: TrieMap[RemoteUser, Brush] = TrieMap[RemoteUser, Brush]()
+  var remoteUsers: TrieMap[ModelObserver, Brush] = TrieMap[ModelObserver, Brush]()
 
   val grid: PixelGrid = PixelGrid(40, 40)
   brushManager.addBrush(brush)
   val view = new PixelGridView(grid, brushManager, 800, 800)
 
-  def newUserJoin(remote: RemoteUser, brush: Brush): Unit = synchronized {
+  def onUserJoin(remote: ModelObserver, brush: Brush): Unit = synchronized {
     remoteUsers.addOne(remote, brush)
     brushManager.addBrush(brush)
     println("user added")
   }
 
-  def userMovedMouse(remote: RemoteUser, x: Int, y: Int): Unit = synchronized {
+  def onMouseMoved(remote: ModelObserver, x: Int, y: Int): Unit = synchronized {
     remoteUsers(remote).updatePosition(x, y)
     view.refresh()
     brushManager.addBrush(brush)
     println("remote position update")
   }
 
-  def userChangeColor(remote: RemoteUser, color: Int): Unit = synchronized {
+  def onUserColorChange(remote: ModelObserver, color: Int): Unit = synchronized {
     remoteUsers(remote).setColor(color)
     view.refresh()
     println("remote color change")
   }
 
-  def color(x: Int, y: Int, color: Int): Unit = synchronized{
+  def onPixelColor(x: Int, y: Int, color: Int): Unit = synchronized{
     grid.set(x, y, color)
     view.refresh()
   }
 
-  def userLeave(remote: RemoteUser): Unit = synchronized {
+  def onUserExit(remote: ModelObserver): Unit = synchronized {
     val removedBrush = remoteUsers.remove(remote)
     removedBrush.foreach(brushManager.removeBrush)
     view.refresh()
@@ -110,7 +110,7 @@ case class UserImpl() extends RemoteUser with LocalUser:
 object User extends App:
   val registry: Registry = LocateRegistry.getRegistry(null)
   val user = UserImpl()
-  val stub = UnicastRemoteObject.exportObject(user, 0).asInstanceOf[RemoteUser]
+  val stub = UnicastRemoteObject.exportObject(user, 0).asInstanceOf[ModelObserver]
   registry.rebind(user.userId.toString, user)
   val model = registry.lookup("modelService").asInstanceOf[ModelService]
 
@@ -123,14 +123,14 @@ object User extends App:
   user.remoteUsers.values.foreach(user.brushManager.addBrush)
 
   user.view.addMouseMovedListener((x: Int, y: Int) => {
-    user.remoteUsers.foreach((u, _) => u.userMovedMouse(user, x, y))
+    user.remoteUsers.foreach((u, _) => u.onMouseMoved(user, x, y))
     user.brush.updatePosition(x, y)
     user.view.refresh()
   })
 
   user.view.addColorChangedListener(color => {
     user.brush.setColor(color)
-    user.remoteUsers.foreach((u, _) => u.userChangeColor(user, color))
+    user.remoteUsers.foreach((u, _) => u.onUserColorChange(user, color))
     model.userChangeColor(user, color)
     user.view.refresh()
   })
@@ -142,7 +142,7 @@ object User extends App:
   private val windowListener = new WindowAdapter() {
     override def windowClosing(e: WindowEvent): Unit =
       model.leave(user)
-      user.remoteUsers.foreach((u, _) => u.userLeave(user))
+      user.remoteUsers.foreach((u, _) => u.onUserExit(user))
       user.view.dispose()
       System.exit(0)
   }
